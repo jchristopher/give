a6e515db78468334130f875c4c35d4a639be644f
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-09-26 12:18:36 +0530

    fix: handle rest reuest for stat updater

--- includes/payments/class-give-stats-background-updater.php
+++ includes/payments/class-give-stats-background-updater.php
@@ -38,12 +38,67 @@ class Give_Stats_Background_Updater extends WP_Background_Process {
 	protected $action = 'stats_updater';
 
 	/**
+	 * Setup
+	 */
+	public function __construct() {
+		$this->identifier               = $this->prefix . '_' . $this->action;
+		$this->cron_hook_identifier     = $this->identifier . '_cron';
+		$this->cron_interval_identifier = $this->identifier . '_cron_interval';
+
+		add_action( "give_$this->identifier", array( $this, 'maybe_handle' ) );
+		add_action( $this->cron_hook_identifier, array( $this, 'handle_cron_healthcheck' ) );
+		add_filter( 'cron_schedules', array( $this, 'schedule_cron_healthcheck' ) );
+	}
+
+	/**
 	 * Get query URL
 	 *
 	 * @return string
 	 */
 	protected function get_query_url() {
 		return home_url( 'give-api/stats-updater' );
+	}
+
+	/**
+	 * Get query args
+	 *
+	 * @return array
+	 */
+	protected function get_query_args() {
+		return array(
+			'give-action' => $this->identifier,
+			'nonce'  => wp_create_nonce( $this->identifier ),
+		);
+	}
+
+	/**
+	 * Maybe process queue
+	 *
+	 * Checks whether data exists within the queue and that
+	 * the process is not already running.
+	 */
+	public function maybe_handle() {
+		// Don't lock up other requests while processing
+		session_write_close();
+
+		if ( $this->is_process_running() ) {
+			// Background process already running.
+			// Create another batch to process in future
+			// @todo: create new batch if cron running already.
+
+			wp_die();
+		}
+
+		if ( $this->is_queue_empty() ) {
+			// No data to process.
+			wp_die();
+		}
+
+		check_ajax_referer( $this->identifier, 'nonce' );
+
+		$this->handle();
+
+		wp_die();
 	}
 
 	/**
@@ -57,14 +112,16 @@ class Give_Stats_Background_Updater extends WP_Background_Process {
 	 * @return array|bool
 	 */
 	protected function task( $item ) {
-		switch ( $item['type'] ) {
-			case 'donor':
-				break;
-
-			case 'form':
-				break;
-		}
+		// switch ( $item['type'] ) {
+		// 	case 'donor':
+		// 		break;
+		//
+		// 	case 'form':
+		// 		break;
+		// }
 
 		return false;
 	}
 }
+
+new Give_Stats_Background_Updater();
